2b3e2d20aaad189dddf5d678d8038e77138f03f9
DragonVelvety <122022351+DragonVelvety@users.noreply.github.com>
2023-05-27 16:48:32 +0200

    Commited my first python scripts

---
+++ session1_setup.py
@@ -0,0 +1,4 @@
+print("Welcome to the python2 programming course")
+
+# ctrl +s - to save
+# ctrl +b - to execute
---
+++ session2_basics.py
@@ -0,0 +1,20 @@
+# firstNumber = 12
+# secondNumber = 14
+# addition = firstNumber + secondNumber
+# print(addition)
+
+# # id function - memory location
+# # type function
+
+# print(type(firstNumber), type(secondNumber))
+# print(id(firstNumber), id(secondNumber))
+
+a = 10
+b = 10
+
+print(id(a), id(b))
+
+# Object intering
+# 1 variable names should start with a lower case character
+# 2 variable names should not begin with a digit (although digits are allowed)
+# 3 _ is allowed in variable names

96036abd61f3d67d6e9bf60516085ba364daf105
DragonVelvety <122022351+DragonVelvety@users.noreply.github.com>
2023-05-27 17:40:29 +0200

    Added datatype script

---
+++ session3.py
@@ -0,0 +1,49 @@
+# Datatypes:
+# 1 int  
+# num1 = 100
+# print(type(num1))
+
+# 2 float
+# num2 = 15.25
+# print(type(num2))
+
+# number1 = 100
+# print(id(number1))
+# number1 = 105
+# print(id(number1))
+
+# integers and floats are immutable data types
+
+# 3 string
+# s = "'Python' sample string"
+# print(id(s))
+# print(s, type(s))
+
+# s = "New value"
+# print(id(s))
+
+# 4 list : []
+# l = [10, 20, 30, 40, 50,"Python","Java"]
+# print(id(l))
+# l.append(60)
+# print(l)
+# print(id(l))
+
+# list is a mutable data type
+
+# 5 tuple : 
+# t = (10,20,30)
+# tuple is an immutable data type
+
+# 6 Dict : dictionary is a mutable data type
+# d = {"name":"ABC","Email":"test@testinger.com"}
+# print(d)
+
+# 7 Set {} set is a mutable data type
+# s = {10,20,30,40}
+
+# 8 bool : True, False
+
+# 9 complex : 4 + 3j
+
+help(str) 

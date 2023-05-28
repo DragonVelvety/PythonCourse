1b71a2b4adf809cf9b4376220199c708a933a91d
DragonVelvety <122022351+DragonVelvety@users.noreply.github.com>
2023-05-28 15:27:31 +0200

    Added python script for operators

---
+++ session4_operators.py
@@ -0,0 +1,61 @@
+# Operators in Python
+# 1 Arithmetic operators +,-,*,/,//,%
+# 2 Comparison operators <,>,<=,>=,==,!=
+# 3 Identity operators is, is not
+# 4 Assignment operators =,+=,-=,*=,/=
+# 5 Bitwise operators &,|,^,>>,<<
+# 6 Logical operators and, or, not 
+# 7 Membership operators in, not in
+
+# number1 = 15
+# number2 = 25
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 * number2)
+
+# print(10/3)
+# print(10//3)
+# print(10%3)
+
+# print(36 ** 5)
+
+# number1 = 50
+# number2 = 150
+
+# print(number1 == number2)
+
+# number1 = 250
+# number2 = 250
+
+# print(number1 == number2)
+# print(number1 is number2)
+
+# l1 = [10,20,30]
+# l2 = [10,20,30]
+# print(l1 == l2)
+# print(l1 is l2)
+# print(l1 is not l2)
+
+# number1 = 200
+# number1 = number1 + 50
+# print(number1)
+# number1 += 15
+# print(number1)
+# number1 -= 25
+# print(number1)
+
+# number1 = 2
+# number2 = 1
+# print(bin(number1), bin(number2))
+# print(number1 | number2)
+# print(bin(2))
+# print(2 << 1)
+
+# print(10 == 15 or 20 == 20)
+# print(not(10==20))
+
+l1 = [10,20,30,40,50]
+print(35 in l1)
+
+s = "Python string"
+print("Python" not in s)
